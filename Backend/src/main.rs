9ce07c51809d02b57d6781bef5c895272e4d3cfc
Jenil Desai <jenildev91@gmail.com>
2025-02-11 14:52:54 +0530

    v1.0.1
    + Rocket.rs Framework Setup
    + JSON Setup

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::serde::{json::Json, Serialize};
+use rocket::{get, launch, routes};
+
+#[derive(Serialize)]
+enum Status {
+    Success,
+    Error,
+}
+#[derive(Serialize)]
+struct IndexResponse {
+    message: String,
+    status: Status,
+}
+
+#[get("/")]
+fn index() -> Json<IndexResponse> {
+    Json(IndexResponse {
+        message: "Hello, world!".to_string(),
+        status: Status::Success,
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
